68781f6afed4cadc7b714083b9175af703b15834
Christian Barrios <christiandbf@hotmail.com>
2017-11-15 15:55:56 -0500

    Finished

--- index.js
+++ index.js
@@ -16,27 +16,29 @@ let subject = config.subject
 let exchanges = config.exchanges.map((exchange) => new ccxt[exchange]())
 
 let currentTickers = exchanges.reduce((acc, exchange) => {
-    acc[exchange.name] = undefined
-    return acc
+  acc[exchange.name] = undefined
+  return acc
 }, {})
 
 setInterval(async () => {
-    exchanges.forEach(async function (exchange) {
-        if (!currentTickers[exchange.name]) {
-            currentTickers[exchange.name] = await exchange.fetchTicker('BTC/USDT')
-            console.log(`Saving the first tickers of ${exchange.name} to compare them later`)
-        } else {
-            let lastTicker = await exchange.fetchTicker('BTC/USDT')
-            if (lastTicker.last < (currentTickers[exchange.name].last - lost)) sendEmail(to, subject, {
-                "exchange": exchange.name,
-                "currentTicker": currentTickers[exchange.name],
-                "lastTicker": lastTicker
-            })
-            console.log(exchange.name, currentTickers[exchange.name].last, lastTicker.last)
-            if (lastTicker.last > currentTickers[exchange.name].last) 
-                currentTickers[exchange.name] = lastTicker
-            if (Date.parse(lastTicker.datetime) - Date.parse(currentTickers[exchange.name].datetime) > check) 
-                currentTickers[exchange.name] = lastTicker
-        }
-    });
+  exchanges.forEach(async function (exchange) {
+    if (!currentTickers[exchange.name]) {
+      currentTickers[exchange.name] = await exchange.fetchTicker('BTC/USDT')
+      console.log(`Saving the first tickers of ${exchange.name} to compare them later`)
+    } else {
+      let lastTicker = await exchange.fetchTicker('BTC/USDT')
+      if (lastTicker.last < (currentTickers[exchange.name].last - lost)) {
+        sendEmail(to, subject, {
+          'exchange': exchange.name,
+          'currentTicker': currentTickers[exchange.name],
+          'lastTicker': lastTicker
+        })
+      }
+      console.log(
+                `${exchange.name}   Last: ${currentTickers[exchange.name].last.toFixed(2)}    Current: ${lastTicker.last.toFixed(2)}`
+            )
+      if (lastTicker.last > currentTickers[exchange.name].last) { currentTickers[exchange.name] = lastTicker }
+      if (Date.parse(lastTicker.datetime) - Date.parse(currentTickers[exchange.name].datetime) > check) { currentTickers[exchange.name] = lastTicker }
+    }
+  })
 }, interval)
